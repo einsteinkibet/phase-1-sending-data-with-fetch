073c84422f32cea8f582f35347f490e33b98635c
einsteinkibet <einsteinkibet0110@gmail.com>
2023-10-05 23:29:03 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ name: name, email: email }),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      // console.log(data);
+      document.querySelector("body").textContent = data.id;
+    })
+    .catch((err) => {
+      // console.log(err);
+      document.querySelector("body").textContent = err;
+    });
+}
+
+submitData("John Doe", "johndoe@gmail.com");
